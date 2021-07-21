929a1c68645e0b482258d7c192d5abfd1e07446b
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-07-21 06:17:42 +0300

    Add files via upload

---
+++ src/AppClass.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import NewTask from "./NewTask";
+import TasksList from "./TasksList";
+
+export default class AppClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTask: {},
+      allTasks: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleChange({ target }){
+    const { name, value } = target;
+    this.setState((prevState) => ({
+      ...prevState,
+      newTask: {
+        ...prevState.newTask,
+        [name]: value,
+        id: Date.now()
+      }
+    }));
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    if (!this.state.newTask.title) return;
+    this.setState((prevState) => ({
+      allTasks: [prevState.newTask, ...prevState.allTasks],
+      newTask: {}
+    }));
+  }
+
+  handleDelete(taskIdToRemove){
+    this.setState((prevState) => ({
+      ...prevState,
+      allTasks: prevState.allTasks.filter((task) => task.id !== taskIdToRemove)
+    }));
+  }
+
+  render() {
+    return (
+      <main>
+        <h1 style={{color: "white", marginTop: "1em", fontFamily: "impact", fontSize: "3em"}}>TODO LIST WITH CLASS COMPONENTS</h1>
+        <NewTask
+          newTask={this.state.newTask}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <TasksList
+          allTasks={this.state.allTasks}
+          handleDelete={this.handleDelete}
+        />
+        <footer>
+          <p>Author: Andres R. Bucheli</p>
+          <p>BUCHELI WEB DEVELOPMENT ©</p>
+          <p><a href="mailto:radio_seattle@hotmail.com">radio_seattle@hotmail.com</a></p>
+        </footer>
+      </main>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./AppClass";
+//import App from "./Container/AppFunction";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+);
